503eb0a211e7d0be2c0d4531cc08486adf6eedf4
Jean-Eudes Patois <exige.exe@gmail.com>
2015-04-17 17:03:14 +0200

    Fin de la classe

--- src/exercice1/Students.java
+++ src/exercice1/Students.java
@@ -2,7 +2,17 @@ package exercice1;
 
 public class Students {
 
+public int getAge() {
+	return this.Age;
+}
 
+public String getLastName() {
+	return this.LastName;
+}
+
+public void setAge(int age) {
+	this.Age=age;
+}
 
 }
 
