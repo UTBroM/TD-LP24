7baff3fb5ca7e5943f4cd4ed9072113d57ec63af
Jean-Eudes Patois <exige.exe@gmail.com>
2015-04-17 17:13:00 +0200

    La classe quoi

--- src/exercice1/GroupOfStudent.java
+++ src/exercice1/GroupOfStudent.java
@@ -2,6 +2,18 @@ package exercice1;
 
 public class GroupOfStudent {
 
+	private Students[] students;
+	private final String name;
+	
+	public GroupOfStudent(int size, String name) {
+		students = new Students[size];
+		this.name=name;
+	}
+	
+	/*public Students getStudent(String name) {
+	
+	}*/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
