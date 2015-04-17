09299c9892374950a013bf53d4c4967899a3cf7a
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-04-17 17:04:25 +0200

    Première partie Students

--- src/exercice1/Students.java
+++ src/exercice1/Students.java
@@ -2,17 +2,26 @@ package exercice1;
 
 public class Students {
 
-public int getAge() {
-	return this.Age;
-}
+	private String lastname;
+	private String firstname;
+	private int age;
+	public Student (String lastname, String firstname, int age){
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.age = age;
+	}
 
-public String getLastName() {
-	return this.LastName;
-}
-
-public void setAge(int age) {
-	this.Age=age;
-}
+	public int getAge() {
+		return this.Age;
+	}
+	
+	public String getLastName() {
+		return this.LastName;
+	}
+	
+	public void setAge(int age) {
+		this.Age=age;
+	}
 
 }
 
